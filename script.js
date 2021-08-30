0ddca17c61dece544314234783c7d64a314e8e8b
eliaraguso <elia.raguso@gmail.com>
2021-08-30 15:09:47 +0200

    creazione scheletro html e css + inizio creazione script JS

---
+++ script.js
@@ -0,0 +1,9 @@
+// 1. Seleziono i bottoni
+var btnGenera = document.getElementById("genera";)
+
+// 2. al click del bottone genera
+btnGenera.addEventListener("click"
+    function() {
+        
+    }
+)
